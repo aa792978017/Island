e517fb20a86d6270da70a5618eea663d597571fa
aa792978017 <792978017@qq.com>
2018-10-01 20:15:47 +0800

    项目创建
    项目创建

---
+++ Island/src/main.js
@@ -0,0 +1,20 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import App from './App'
+import router from './router'
+// 引入mint-ui组件
+import MintUI from 'mint-ui'
+import 'mint-ui/lib/style.css'
+// 引入Echart组件,以后再引入把
+
+Vue.config.productionTip = false
+Vue.use(MintUI)
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  components: { App },
+  template: '<App/>'
+})
